01138f6cb8a0bbb0cd4fdbfe2f730b462a5b8a24
prajwalupadhye056-ui <prajwal.upadhye056@gmail.com>
2025-10-09 23:43:06 +0530

    New Files added

---
+++ src/ex_01_Python_Basics/Lab003_print.py
@@ -0,0 +1,10 @@
+print("Hello World")
+
+# print(self, *args, sep=' ', end='\n', file=None)
+# self -Ignore now
+# *args- unlimited arguments comma separated arguments
+print("Pramod Dutta", " Prajwal", 123, True, 3.14)
+print("Pramod Dutta", "Prajwal", 123, True, 3.14, sep='*')
+print("Pramod Dutta", "Prajwal", 123, True, 3.14, sep='-')
+print("Pramod Dutta", " Prajwal", 123, True, 3.14, sep="*", end="_")
+print("Pramod Dutta", "Prajwal", 123, True, 3.14, sep='-')
