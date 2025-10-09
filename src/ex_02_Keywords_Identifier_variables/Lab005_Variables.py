f4514147d2be59f9d3526e7ff8a29ed855ca56bb
prajwalupadhye056-ui <prajwal.upadhye056@gmail.com>
2025-10-09 23:53:07 +0530

    9th OCT 2025 -New files Added

--- src/ex_02_Keywords_Identifier_variables/Lab005_Variables.py
+++ src/ex_02_Keywords_Identifier_variables/Lab005_Variables.py
@@ -3,3 +3,12 @@ age= "Prajwal"
 print(age)
 
 #Dynamic typed language
+
+pi = 3.14
+name = "Prajwal"
+name = "Prajwal"
+name = "Prajwal"
+name = "Prajwal"
+name = "Prajwal"
+name = "Upadhye"
+print(name)
