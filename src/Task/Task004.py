98ee8c42a705dac15696424013391113fa644963
prajwalupadhye056-ui <prajwal.upadhye056@gmail.com>
2025-10-14 20:36:50 +0530

    14th OCT 2025 -New files Added

---
+++ src/Task/Task004.py
@@ -0,0 +1,13 @@
+# Write a Python program to calculate the area of a triangle using the formula
+
+# Area =1/2 * base * height
+# Take base =10 and height = 5 , print the result using arithmetic operators
+
+Base= float(input("Enter the base \n"))
+print(Base)
+
+Height= float(input("Enter the height \n"))
+print(Height)
+
+area= 1/2 * Base * Height
+print("The area of triangle is : ",area)
---
+++ src/Task/Task005.py
@@ -0,0 +1,5 @@
+# Write a program to check if a number entered by the user is greater than 100.
+
+num1 = float(input("Enter any number \n"))
+
+print("The entered number is greater than 100" if num1 > 100 else "The entered number is less than 100 ")
