96b7ca6cdc3e37b1bfa6b52c0d149369132429ed
prajwalupadhye056-ui <prajwal.upadhye056@gmail.com>
2025-11-27 23:33:35 +0530

    27th Nov 2025 -New files Added

---
+++ src/ex_19_Exceptions/Lab_171_Exception_Group.py
@@ -0,0 +1,12 @@
+#python 3.11
+
+eg = ExceptionGroup("Multiple Ex", [
+ ValueError("Invalid Value"),
+ TypeError("Type Error"),
+ ZeroDivisionError("Can't div zero")
+
+])
+
+def check_div(a):
+    if a==0:
+        raise eg
